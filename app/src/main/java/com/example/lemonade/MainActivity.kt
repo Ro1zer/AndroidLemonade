bcb0076d81e53bb7c9ecc5511c5905d670e1a786
Maksym Oleksiienko <starsredmaks@gmail.com>
2025-04-10 16:21:14 +0300

    Add some code refactor after seen results

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -7,18 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,11 +29,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
@@ -50,91 +50,88 @@ class MainActivity : ComponentActivity() {
 
 enum class Lemonade { TREE, LEMON, JUICE, REPEAT; }
 
-@Preview
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-
-        Box(
-            contentAlignment = Alignment.TopCenter
-        ) {
-            Text(
-                modifier = Modifier
-                    .background(Color(0xFFFFEB3B))
-                    .fillMaxWidth()
-                    .padding(top = 36.dp, bottom = 20.dp),
-                textAlign = TextAlign.Center,
-                text = stringResource(R.string.app_name),
-                fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                fontWeight = FontWeight.SemiBold
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.app_name), fontWeight = FontWeight.Bold
+                    )
+                }, colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = Color.Yellow
+                )
             )
-        }
-        var state by remember { mutableStateOf(Lemonade.TREE) }
-        var randomCount = (0..10).random()
-        var clickCounter = 0
-        var image = when (state) {
-            Lemonade.TREE -> painterResource(R.drawable.lemon_tree)
-            Lemonade.LEMON -> painterResource(R.drawable.lemon_squeeze)
-            Lemonade.JUICE -> painterResource(R.drawable.lemon_drink)
-            else -> painterResource(R.drawable.lemon_restart)
-        }
-        var imageDesc = when (state) {
-            Lemonade.TREE -> "Lemon tree"
-            Lemonade.LEMON -> "Lemon"
-            Lemonade.JUICE -> "Glass of lemonade"
-            else -> "Empty glass"
-        }
-        var desc = when (state) {
-            Lemonade.TREE -> stringResource(R.string.lemon_tree_desc)
-            Lemonade.LEMON -> stringResource(R.string.lemon_desc)
-            Lemonade.JUICE -> stringResource(R.string.glass_of_lemonade_desc)
-            else -> stringResource(R.string.empty_glass_of_lemonade)
-        }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+        }) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
         ) {
-            LemonadeButton(image, imageDesc) {
-                when (state) {
-                    Lemonade.TREE -> {
-                        state = Lemonade.LEMON
-                    }
-                    Lemonade.LEMON -> {
-                        if (clickCounter == randomCount) {
-                            state = Lemonade.JUICE
-                            randomCount = (0..10).random()
-                            clickCounter = 0
-                        } else {
-                            clickCounter++
+            var state by remember { mutableStateOf(Lemonade.TREE) }
+            var squeezeCount by remember { mutableStateOf(0) }
+            var imageId = when (state) {
+                Lemonade.TREE -> R.drawable.lemon_tree
+                Lemonade.LEMON -> R.drawable.lemon_squeeze
+                Lemonade.JUICE -> R.drawable.lemon_drink
+                else -> R.drawable.lemon_restart
+            }
+            var imageDescId = when (state) {
+                Lemonade.TREE -> R.string.lemon_tree_image_desc
+                Lemonade.LEMON -> R.string.lemon_image_desc
+                Lemonade.JUICE -> R.string.glass_of_lemonade_image_desc
+                else -> R.string.empty_glass_of_lemonade_image_desc
+            }
+            var descId = when (state) {
+                Lemonade.TREE -> R.string.lemon_tree_desc
+                Lemonade.LEMON -> R.string.lemon_desc
+                Lemonade.JUICE -> R.string.glass_of_lemonade_desc
+                else -> R.string.empty_glass_of_lemonade_desc
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                LemonadeButton(imageId, imageDescId) {
+                    when (state) {
+                        Lemonade.TREE -> {
+                            state = Lemonade.LEMON
+                            squeezeCount = (2..4).random()
+                        }
+
+                        Lemonade.LEMON -> {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                state = Lemonade.JUICE
+                            }
+                        }
+
+                        Lemonade.JUICE -> {
+                            state = Lemonade.REPEAT
+                        }
+
+                        else -> {
+                            state = Lemonade.TREE
                         }
                     }
-                    Lemonade.JUICE -> {
-                        state = Lemonade.REPEAT
-                    }
-
-                    else -> {
-                        state = Lemonade.TREE
-                    }
                 }
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    text = stringResource(descId),
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
-            Spacer(Modifier.height(16.dp))
-            Text(
-                text = desc,
-                color = MaterialTheme.colorScheme.inverseSurface,
-                fontSize = MaterialTheme.typography.titleMedium.fontSize
-            )
         }
     }
 }
 
 @Composable
 fun LemonadeButton(
-    image: Painter,
-    imageDescription: String,
-    action: () -> Unit
+    imageId: Int, imageDescriptionId: Int, action: () -> Unit
 ) {
     Button(
         shape = MaterialTheme.shapes.extraLarge,
@@ -142,9 +139,17 @@ fun LemonadeButton(
         onClick = action,
     ) {
         Image(
-            painter = image,
-            contentDescription = imageDescription,
+            painter = painterResource(imageId),
+            contentDescription = stringResource(imageDescriptionId),
             alignment = Alignment.Center,
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LemonadePreview() {
+    LemonadeTheme {
+        App()
     }
 }
